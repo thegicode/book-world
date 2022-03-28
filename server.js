da066bea9dfc794fd3e05876464de9cfeaf19904
thegi.code <thegi.code@gmail.com>
2022-03-28 22:32:33 +0900

    Update

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
+const axios = require('axios').default;
 
-console.log(__dirname)
+const key = require("./user.js").user
+
 app.use(express.static(`${__dirname}/src/`));
+
+app.get('/api', function(req, res) {
+
+  const url1 = `http://data4library.kr/api/srchDtlList?authKey=${key}&isbn13=9788960532410&&format=json`
+  const url2 = `http://data4library.kr/api/bookExist?authKey=${key}&isbn13=9788960532410&libCode=111007&format=json`
+
+  const requestOne = axios.get(url1)
+  const requestTwo = axios.get(url2)
+
+  axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+    const responseOne = responses[0].data.response.detail[0].book
+    const responseTwo = responses[1].data.response
+    const { libCode } = responseTwo.request
+    res.send({...responseOne, libCode, ...responseTwo.result})
+  })).catch(errors => {
+  })
+
+});
 
 app.listen(port, () => {
   console.log(`Start : http://localhost:${port}`);
 });
+
+
+// 111007 고덕도서관
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,56 @@
-console.log('index');
+import BookList from './BookList.js'
+import TestList from './TestList.js'
+
+customElements.define('book-list', BookList)
+customElements.define('test-list', TestList)
+
+
+fetch('/api', {
+    method: 'GET'
+})
+.then((data) => data.json()) 
+.then(response => {
+    const {authors,
+        bookImageURL,
+        bookname,
+        class_nm,
+        class_no,
+        description,
+        isbn,
+        isbn13,
+        no,
+        publication_date,
+        publication_year,
+        publisher, 
+        libCode,
+        hasBook,
+        loanAvailable} = response
+    document.querySelector('.book')
+        .innerHTML = `
+        <div>${authors}</div>
+        <div><img src="${bookImageURL}"></div>
+        <div>${bookname}</div>
+        <div>${class_nm}</div>
+        <div>${class_no}</div>
+        <div>${description}</div>
+        <div>${isbn}</div>
+        <div>${isbn13}</div>
+        <div>${no}</div>
+        <div>${publication_date}</div>
+        <div>${publication_year}</div>
+        <div>${publisher}</div>
+        
+        `
+    document.querySelector('.lib')
+        .innerHTML = `
+            <div>도선관 코드 : ${libCode}</div>
+            <div>소장여부: ${hasBook}</div>
+            <div>대출여부: ${loanAvailable}</div>`
+
+})
+.catch(e => {
+    console.log(e);
+});
+
+
+
