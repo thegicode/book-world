de64191b46d89c9f6a2591123d171079fad485ff
thegi.code <thegi.code@gmail.com>
2022-03-31 22:06:15 +0900

    Add naver api

--- server.js
+++ server.js
@@ -3,53 +3,77 @@ const app = express();
 const port = 8000;
 const axios = require('axios').default;
 
-const key = require("./user.js").user
-
 app.use(express.static(`${__dirname}/src/`));
 
-const host = 'http://data4library.kr/api'
-const authKey = `authKey=${key}`
-const foramt = 'format=json'
-
-app.get('/a', function(req, res) {
-    const { isbn13, libCode } = req.query
-    
-    const isbn = `isbn13=${isbn13}`
-    const lib = `libCode=${libCode}`
-
-    const url = `${host}/itemSrch?type=ALL&${lib}&${authKey}&pageNo=1&pageSize=10&${foramt}`
-
-    axios.get(url)
-        .then( response => {
-            console.log(response.data)
-            // const { resultNum, items } = response.data.response
-            // console.log(resultNum, items)
-        })
-        .catch(errors => {
-            console.log(error)
-        })
-});
+const { libKey, naverKey } = require("./user.js")
 
 
-app.get('/b', function(req, res) {
-    const { isbn13, libCode } = req.query
-    const isbn = `isbn13=${isbn13}`
-    const lib = `libCode=${libCode}`
-
-    const url1 = `${host}/srchDtlList?${authKey}&${isbn}&&${foramt}`
-    const url2 = `${host}/bookExist?${authKey}&${isbn}&${lib}&${foramt}`
-
-    const requestOne = axios.get(url1)
-    const requestTwo = axios.get(url2)
-
-    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
-        const responseOne = responses[0].data.response.detail[0].book
-        const responseTwo = responses[1].data.response
-        const { libCode } = responseTwo.request
-        res.send({...responseOne, libCode, ...responseTwo.result})
-    })).catch(errors => {
+app.get('/naver', function(req, res) {
+    const { name, display, start } = req.query
+    axios.get(`https://openapi.naver.com/v1/search/book.json`, {
+        params: {
+            query: name, 
+            display, 
+            start
+        },
+        headers: {
+            "X-Naver-Client-Id": naverKey.clientID,
+            "X-Naver-Client-Secret": naverKey.clientSecret
+        }
+    })
+    .then( response => {
+        const { total, start, display, items } = response.data
+        res.send({total, start, display, items})
+    })
+    .catch(errors => {
+        console.log('naver error', error)
     })
 });
+
+
+
+// const host = 'http://data4library.kr/api'
+// const authKey = `authKey=${libKey}`
+// const foramt = 'format=json'
+
+// app.get('/a', function(req, res) {
+//     const { isbn13, libCode } = req.query
+    
+//     const isbn = `isbn13=${isbn13}`
+//     const lib = `libCode=${libCode}`
+
+//     const url = `${host}/itemSrch?type=ALL&${lib}&${authKey}&pageNo=1&pageSize=10&${foramt}`
+
+//     axios.get(url)
+//         .then( response => {
+//             console.log(response.data.response)
+//             res.send(response.data.response)
+//         })
+//         .catch(errors => {
+//             console.log(error)
+//         })
+// });
+
+
+// app.get('/b', function(req, res) {
+//     const { isbn13, libCode } = req.query
+//     const isbn = `isbn13=${isbn13}`
+//     const lib = `libCode=${libCode}`
+
+//     const url1 = `${host}/srchDtlList?${authKey}&${isbn}&&${foramt}`
+//     const url2 = `${host}/bookExist?${authKey}&${isbn}&${lib}&${foramt}`
+
+//     const requestOne = axios.get(url1)
+//     const requestTwo = axios.get(url2)
+
+//     axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+//         const responseOne = responses[0].data.response.detail[0].book
+//         const responseTwo = responses[1].data.response
+//         const { libCode } = responseTwo.request
+//         res.send({...responseOne, libCode, ...responseTwo.result})
+//     })).catch(errors => {
+//     })
+// });
 
 app.listen(port, () => {
     console.log(`Start : http://localhost:${port}`);
--- src/js/index.js
+++ src/js/index.js
@@ -7,56 +7,89 @@ customElements.define('test-list', TestList)
 const isbn13 = '9788960532410'
 const libCode = '111007'
 
-fetch(`/a?isbn13=${isbn13}&libCode=${libCode}`, {
+
+
+fetch(`/naver?name=${'토지'}&display=${10}&start=${1}`, {
     method: 'GET'
 })
-.then( data => console.log(data))
-
-
-fetch(`/b?isbn13=${isbn13}&libCode=${libCode}`, {
-    method: 'GET'
-})
-.then((data) => data.json()) 
-.then(response => {
-    const {authors,
-        bookImageURL,
-        bookname,
-        class_nm,
-        class_no,
-        description,
-        isbn,
-        isbn13,
-        no,
-        publication_date,
-        publication_year,
-        publisher, 
-        libCode,
-        hasBook,
-        loanAvailable} = response
-    document.querySelector('.book')
-        .innerHTML = `
-        <div>${authors}</div>
-        <div><img src="${bookImageURL}"></div>
-        <div>${bookname}</div>
-        <div>${class_nm}</div>
-        <div>${class_no}</div>
-        <div>${description}</div>
-        <div>${isbn}</div>
-        <div>${isbn13}</div>
-        <div>${no}</div>
-        <div>${publication_date}</div>
-        <div>${publication_year}</div>
-        <div>${publisher}</div>`
-    document.querySelector('.lib')
-        .innerHTML = `
-            <div>도선관 코드 : ${libCode}</div>
-            <div>소장여부: ${hasBook}</div>
-            <div>대출여부: ${loanAvailable}</div>`
-
+.then( data => data.json())
+.then( response => {
+    console.log(response)
+    const { total, start, display, items } = response
+    const names = items.map( item => item.title).join('</p><p> ')
+    document.querySelector('.ly-2')
+        .innerHTML =`
+            <div>total: ${total}</div>
+            <div>start: ${start}</div>
+            <div>display: ${display}</div>
+            <p>${names}</p>
+        `
 })
 .catch(e => {
     console.log(e);
 });
+
+
+
+
+// fetch(`/a?isbn13=${isbn13}&libCode=${libCode}`, {
+//     method: 'GET'
+// })
+// .then( data => data.json())
+// .then( response => {
+//     console.log(response)
+// })
+// .catch(e => {
+//     console.log(e);
+// });
+
+
+
+
+// fetch(`/b?isbn13=${isbn13}&libCode=${libCode}`, {
+//     method: 'GET'
+// })
+// .then((data) => data.json()) 
+// .then(response => {
+//     const {authors,
+//         bookImageURL,
+//         bookname,
+//         class_nm,
+//         class_no,
+//         description,
+//         isbn,
+//         isbn13,
+//         no,
+//         publication_date,
+//         publication_year,
+//         publisher, 
+//         libCode,
+//         hasBook,
+//         loanAvailable} = response
+//     document.querySelector('.book')
+//         .innerHTML = `
+//         <div>${authors}</div>
+//         <div><img src="${bookImageURL}"></div>
+//         <div>${bookname}</div>
+//         <div>${class_nm}</div>
+//         <div>${class_no}</div>
+//         <div>${description}</div>
+//         <div>${isbn}</div>
+//         <div>${isbn13}</div>
+//         <div>${no}</div>
+//         <div>${publication_date}</div>
+//         <div>${publication_year}</div>
+//         <div>${publisher}</div>`
+//     document.querySelector('.lib')
+//         .innerHTML = `
+//             <div>도선관 코드 : ${libCode}</div>
+//             <div>소장여부: ${hasBook}</div>
+//             <div>대출여부: ${loanAvailable}</div>`
+
+// })
+// .catch(e => {
+//     console.log(e);
+// });
 
 
 
