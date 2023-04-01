d674fc800862b35c493f1370bc893710bcb4803a
thegicode <thegi.code@gmail.com>
2023-04-01 22:45:43 +0900

    Refactor server/apiHandlers

--- server/apiHandlers.ts
+++ server/apiHandlers.ts
@@ -5,9 +5,16 @@ import dotenv from "dotenv";
 dotenv.config({ path: path.resolve(__dirname, ".env.key") });
 const { LIBRARY_KEY, NAVER_CLIENT_ID, NAVER_SECRET_KEY } = process.env;
 
-/** NAVER */
+const fetchData = async (url: string, headers?: Record<string, string>) => {
+    const response = await fetch(url, { headers });
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
 
-// 키워드 검색
+    return await response.json();
+};
+
+// NAVER ; 키워드 검색
 export async function searchNaverBook(req: Request, res: Response) {
     const { keyword, display, start } = req.query;
     const queryParams = new URLSearchParams({
@@ -21,13 +28,7 @@ export async function searchNaverBook(req: Request, res: Response) {
     };
     const url = `https://openapi.naver.com/v1/search/book.json?${queryParams}`;
     try {
-        const response = await fetch(url, { headers });
-        if (!response.ok) {
-            throw new Error(
-                `Failed to get books from Naver: ${response.statusText}`
-            );
-        }
-        const data = await response.json();
+        const data = await fetchData(url, headers);
         const { total, start, display, items } = data;
         res.send({ total, start, display, items });
     } catch (error) {
@@ -37,28 +38,28 @@ export async function searchNaverBook(req: Request, res: Response) {
 }
 
 /** 도서관 정보나루 */
-const host = "http://data4library.kr/api";
-const authKey = `authKey=${LIBRARY_KEY}`;
-const formatType = "json";
+const buildLibraryApiUrl = (
+    apiPath: string,
+    params: Record<string, string>
+) => {
+    const queryParams = new URLSearchParams({
+        ...params,
+        authKey: LIBRARY_KEY as string,
+        format: "json",
+    });
+    return `http://data4library.kr/api/${apiPath}?${queryParams}`;
+};
 
 // 정보공개 도서관 조회
 export async function librarySearch(req: Request, res: Response) {
     const { dtl_region, page, pageSize } = req.query;
-    const queryParams = new URLSearchParams({
+    const url = buildLibraryApiUrl("libSrch", {
         dtl_region: dtl_region as string,
         pageNo: page as string,
         pageSize: pageSize as string,
-        format: formatType as string,
     });
-    const url = `${host}/libSrch?${authKey}&${queryParams}`;
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(
-                `Failed to get library search data: ${response.statusText}`
-            );
-        }
-        const data = await response.json();
+        const data = await fetchData(url);
         const { pageNo, pageSize, numFound, resultNum, libs } = data.response;
         const formattedData = {
             pageNo,
@@ -77,23 +78,13 @@ export async function librarySearch(req: Request, res: Response) {
 // 지정 도서관, 책 소장 & 대출 가능 여부
 export async function bookExist(req: Request, res: Response) {
     const { isbn13, libCode } = req.query;
-    const queryParams = new URLSearchParams({
+    const url = buildLibraryApiUrl("bookExist", {
         isbn13: isbn13 as string,
         libCode: libCode as string,
-        format: formatType as string,
     });
-    const url = `${host}/bookExist?${authKey}&${queryParams}`;
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(
-                `Failed to check book exist data: ${response.statusText}`
-            );
-        }
-        const data = await response.json();
+        const data = await fetchData(url);
         const result = data.response?.result;
-        // console.log(result)
-
         res.send(result);
     } catch (error) {
         console.error(error);
@@ -104,20 +95,12 @@ export async function bookExist(req: Request, res: Response) {
 // 도서별 이용 분석
 export async function usageAnalysisList(req: Request, res: Response) {
     const { isbn13 } = req.query;
-    const params = new URLSearchParams({
+    const url = buildLibraryApiUrl("usageAnalysisList", {
         isbn13: isbn13 as string,
         loaninfoYN: "Y" as string,
-        format: formatType as string,
     });
-    const url = `${host}/usageAnalysisList?${authKey}&${params}`;
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(
-                `Failed to get usage analysis list data: ${response.statusText} `
-            );
-        }
-        const data = await response.json();
+        const data = await fetchData(url);
         const { book, loanHistory, loanGrps, keywords, recBooks, coLoanBooks } =
             data.response;
 
@@ -154,21 +137,13 @@ export async function usageAnalysisList(req: Request, res: Response) {
 // 도서 소장 도서관 조회
 export async function librarySearchByBook(req: Request, res: Response) {
     const { isbn, region, dtl_region } = req.query;
-    const params = new URLSearchParams({
+    const url = buildLibraryApiUrl("libSrchByBook", {
         isbn: isbn as string,
         region: region as string,
         dtl_region: dtl_region as string,
-        format: formatType as string,
     });
-    const url = `${host}/libSrchByBook?${authKey}&${params}`;
     try {
-        const response = await fetch(url, { method: "GET" });
-        if (!response.ok) {
-            throw new Error(
-                `Failed to get library data: ${response.statusText}`
-            );
-        }
-        const data = await response.json();
+        const data = await fetchData(url, { method: "GET" });
         const { pageNo, pageSize, numFound, resultNum, libs } = data.response;
         res.send({
             pageNo,
@@ -182,14 +157,6 @@ export async function librarySearchByBook(req: Request, res: Response) {
         res.status(500).send("Failed to get library data");
     }
 }
-
-// module.exports = {
-//     searchNaverBook,
-//     librarySearch,
-//     bookExist,
-//     usageAnalysisList,
-//     librarySearchByBook,
-// };
 
 // export function searchNaverBook(arg0: string, searchNaverBook: any) {
 //     throw new Error("Function not implemented.");
