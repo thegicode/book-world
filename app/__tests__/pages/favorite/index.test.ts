3577339887e6ea7f193a532a618b29cef29a963c
thegicode <thegi.code@gmail.com>
2023-06-16 22:57:17 +0900

    Update favorite/index.test.ts

--- app/__tests__/pages/favorite/index.test.ts
+++ app/__tests__/pages/favorite/index.test.ts
@@ -1,34 +1,13 @@
-import {
-    BookDescription,
-    CheckboxFavoriteBook,
-    LibraryBookExist,
-    NavGnb,
-    BookImage,
-} from "../../../scripts/components/index";
-
-import Favorite from "../../../scripts/pages/favorite/Favorite";
-import FavoriteItem from "../../../scripts/pages/favorite/FavoriteItem";
+import "../../../scripts/pages/favorite";
 
 describe("pages favorite index", () => {
-    test("NavGnb is defined", () => {
-        expect(NavGnb).toBeDefined();
-    });
-    test("Favorite is defined", () => {
-        expect(Favorite).toBeDefined();
-    });
-    test("FavoriteItem is defined", () => {
-        expect(FavoriteItem).toBeDefined();
-    });
-    test("BookDescription is defined", () => {
-        expect(BookDescription).toBeDefined();
-    });
-    test("LibraryBookExist is defined", () => {
-        expect(LibraryBookExist).toBeDefined();
-    });
-    test("CheckboxFavoriteBook is defined", () => {
-        expect(CheckboxFavoriteBook).toBeDefined();
-    });
-    test("BookImage is defined", () => {
-        expect(BookImage).toBeDefined();
+    test("custom elements should be defined", () => {
+        expect(customElements.get("nav-gnb")).toBeDefined();
+        expect(customElements.get("app-favorite")).toBeDefined();
+        expect(customElements.get("favorite-item")).toBeDefined();
+        expect(customElements.get("book-description")).toBeDefined();
+        expect(customElements.get("library-book-exist")).toBeDefined();
+        expect(customElements.get("checkbox-favorite-book")).toBeDefined();
+        expect(customElements.get("book-image")).toBeDefined();
     });
 });
