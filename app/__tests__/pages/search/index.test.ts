77f1d488219fe5f4f6a66fc1fcc551df6b632124
thegicode <thegi.code@gmail.com>
2023-06-29 20:09:02 +0900

    Create serach index.test.ts

---
+++ app/__tests__/pages/search/index.test.ts
@@ -0,0 +1,15 @@
+import "../../../scripts/pages/search";
+
+describe("pages search index", () => {
+    test("custom elements should be defined", () => {
+        expect(customElements.get("nav-gnb")).toBeDefined();
+        expect(customElements.get("book-list")).toBeDefined();
+        expect(customElements.get("app-search")).toBeDefined();
+        expect(customElements.get("input-search")).toBeDefined();
+        expect(customElements.get("book-item")).toBeDefined();
+        expect(customElements.get("book-description")).toBeDefined();
+        expect(customElements.get("library-book-exist")).toBeDefined();
+        expect(customElements.get("checkbox-favorite-book")).toBeDefined();
+        expect(customElements.get("book-image")).toBeDefined();
+    });
+});
