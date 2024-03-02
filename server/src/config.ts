c8e9e05fb8d12445163902943212289aa923f772
thegicode <thegi.code@gmail.com>
2024-03-03 00:29:45 +0900

    move server files

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,13 +2,13 @@ import path from "path";
 import dotenv from "dotenv";
 
 const isProduction = process.env.NODE_ENV === "production";
-const envFile = isProduction ? ".env.production" : ".env.development";
+const envFile = isProduction ? "../.env.production" : "../.env.development";
 const buildPath = isProduction ? "app/build" : "app/public";
-const rootPath = path.join(__dirname, "..");
+const rootPath = path.join(__dirname, "../../");
 const destinationPath = path.join(rootPath, buildPath);
 
 dotenv.config({ path: path.resolve(__dirname, envFile) });
-dotenv.config({ path: path.resolve(__dirname, ".env.key") });
+dotenv.config({ path: path.resolve(__dirname, "../.env.key") });
 
 const { PORT } = process.env;
 
