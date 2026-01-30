71ecd844d1ade457696036749ca90683517f06b9
thegicode <thegi.code@gmail.com>
2026-01-30 23:41:35 +0900

    Fix: Remove unused NextFunction import in apiController.ts

--- server/src/controllers/apiController.ts
+++ server/src/controllers/apiController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { AppError } from "../utils/AppError";
 import * as BookService from "../apis";
